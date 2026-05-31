b8d44c7f381b54a3e24ac33947195e467a81be26
Yasasri44 <23eg105n42@anurag.edu.in>
2026-05-31 10:20:47 +0530

    Project setup completed

---
+++ backend/placementpilot/src/main/java/com/yasasri/placementpilot/PlacementpilotApplication.java
@@ -0,0 +1,13 @@
+package com.yasasri.placementpilot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlacementpilotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlacementpilotApplication.class, args);
+	}
+
+}
---
+++ backend/placementpilot/src/test/java/com/yasasri/placementpilot/PlacementpilotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yasasri.placementpilot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlacementpilotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
